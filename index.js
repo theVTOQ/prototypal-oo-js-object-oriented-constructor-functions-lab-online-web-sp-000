fbd37ec001f2bf9dd57fdc743c24b26a508f8cb1
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-06-10 16:47:37 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function Scooter(year, color, model){
+  return {
+    year,
+    color,
+    model
+  }
+}
+
+function Driver(name, age, experience){
+  return {
+    name,
+    age,
+    experience
+  }
+}
+
+function PickupLocation(address, city){
+  return {
+    address,
+    city
+  }
+}
